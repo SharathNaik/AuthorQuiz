88d9681819b8ad4ad13206f18d5f3a68ec18f11e
SharathNaik <nayak.sharathm@gmail.com>
2019-07-21 11:25:58 +0530

    Updates

---
+++ precache-manifest.79903f32ce697d0f110be97a659c1732.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd8689f080560aebaf9bc1b0bac2afd7",
+    "url": "/AuthorQuiz/index.html"
+  },
+  {
+    "revision": "d8da78fdb9fad8f0c9f8",
+    "url": "/AuthorQuiz/static/css/main.14186c94.chunk.css"
+  },
+  {
+    "revision": "86cd9e1e618aa54bdbca",
+    "url": "/AuthorQuiz/static/js/2.ec9c5039.chunk.js"
+  },
+  {
+    "revision": "d8da78fdb9fad8f0c9f8",
+    "url": "/AuthorQuiz/static/js/main.adc28ef1.chunk.js"
+  },
+  {
+    "revision": "ba0040e62579613d99c0",
+    "url": "/AuthorQuiz/static/js/runtime~main.500bd57c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AuthorQuiz/static/media/logo.5d5d9eef.svg"
+  }
+]);
